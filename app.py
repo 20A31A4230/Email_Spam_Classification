46e8641ad928896d2d13f7d9ee6aa058b244fb70
20A31A4230 <kishorekumar110102@gmail.com>
2024-03-23 17:38:26 +0530

    ss

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-
+nltk.download('punkt')
+nltk.download('stopwords')
 ps = PorterStemmer()
 
 
--- setup.sh
+++ setup.sh
@@ -7,5 +7,3 @@ enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
-
-python -m nltk.downloader punkt
